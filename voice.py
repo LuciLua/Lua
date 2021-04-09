061675174f1c042d453875c427edce662c5d620b
LuciLua <luci.lua81@gmail.com>
2021-04-09 15:23:04 -0300

    Teste Pull | ChatVoice

--- voice.py
+++ voice.py
@@ -11,8 +11,8 @@ import socket
 receiver = AudioReceiver('192.168.0.110', 9999)
 receiver_thread = threading.Thread(target=receiver.start_server)
 
-sender = AudioSender('192.168.0.172', 5555)
-sender_thread = threading.Thread(target=sender.start_stream)
+#sender = AudioSender('192.168.0.172', 5555)
+#sender_thread = threading.Thread(target=sender.start_stream)
 
 receiver_thread.start()
-sender_thread.start()
+#sender_thread.start()
